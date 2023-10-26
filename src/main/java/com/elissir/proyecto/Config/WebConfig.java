885d9fb25eed8b2bf9ea8ae4d97f45efe3430447
David Fernando Pérez Medina <davidfernando1112@gmail.com>
2023-10-25 23:14:56 -0500

    Fix: configuracion Cors

---
+++ src/main/java/com/elissir/proyecto/Config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.elissir.proyecto.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(false).maxAge(3600);
+        
+    }
+}
